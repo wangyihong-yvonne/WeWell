d81c2747934027eea4e0ea31a5f8633f33ae8c58
yu <feng.yu1@northeastern.edu>
2021-03-28 20:21:55 -0700

    restart activity after permission granted

--- app/src/main/java/edu/neu/madcourse/wewell/MainActivity.java
+++ app/src/main/java/edu/neu/madcourse/wewell/MainActivity.java
@@ -82,7 +82,7 @@ public class MainActivity extends AppCompatActivity {
                 // If request is cancelled, the result arrays are empty.
                 if (grantResults.length > 0
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    fragment2.onRequestPermissionsResult(requestCode, permissions, grantResults);
+                    recreate();
                 }
             }
         }
