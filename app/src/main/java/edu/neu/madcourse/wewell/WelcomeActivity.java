fccef6d539a47e218130475207445b4fe3eb1a3a
yu <feng.yu1@northeastern.edu>
2021-03-31 13:57:03 -0700

    add welcome activity

---
+++ app/src/main/java/edu/neu/madcourse/wewell/WelcomeActivity.java
@@ -0,0 +1,28 @@
+package edu.neu.madcourse.wewell;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WelcomeActivity extends AppCompatActivity {
+    Handler handler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.welcome);
+
+        handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(WelcomeActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+
+    }
+}
