2c6ba2d5ad77bdb17b24eebdded0ae3073f8d812
wangyihong <yihongwang001@outlook.com>
2021-04-01 16:52:58 -0700

    Update SignInActivity.java

--- app/src/main/java/edu/neu/madcourse/wewell/SignInActivity.java
+++ app/src/main/java/edu/neu/madcourse/wewell/SignInActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -28,6 +29,7 @@ public class SignInActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         FirebaseAuth auth = FirebaseAuth.getInstance();
+        FirebaseUser user = auth.getCurrentUser();
         if (auth.getCurrentUser() != null) {
             // already signed in
             Intent intent = new Intent(SignInActivity.this, MainActivity.class);
@@ -39,38 +41,105 @@ public class SignInActivity extends AppCompatActivity {
         }
     }
 
+
     public void createSignInIntent() {
         // [START auth_fui_create_intent]
         // Choose authentication providers
         List<AuthUI.IdpConfig> providers = Arrays.asList(
                 new AuthUI.IdpConfig.EmailBuilder().build()
+//                .setActionCodeSettings(actionCodeSettings).build()
 //                new AuthUI.IdpConfig.PhoneBuilder().build()
 //                new AuthUI.IdpConfig.GoogleBuilder().build(),
 //                new AuthUI.IdpConfig.FacebookBuilder().build(),
 //                new AuthUI.IdpConfig.TwitterBuilder().build()
         );
 
+        // [START auth_build_action_code_settings]
+//        ActionCodeSettings actionCodeSettings =
+//                ActionCodeSettings.newBuilder()
+//                        // URL you want to redirect back to. The domain (www.example.com) for this
+//                        // URL must be whitelisted in the Firebase Console.
+//                        .setUrl("https://wewell.page.link")
+//                        // This must be true
+//                        .setHandleCodeInApp(true)
+//                        .setIOSBundleId("com.example.ios")
+//                        .setAndroidPackageName(
+//                                "edu.neu.madcourse.wewell",
+//                                true, /* installIfNotAvailable */
+//                                "12"    /* minimumVersion */)
+//                        .build();
+        // [END auth_build_action_code_settings]
 
         // Create and launch sign-in intent
         startActivityForResult(
                 AuthUI.getInstance()
                         .createSignInIntentBuilder()
                         .setAvailableProviders(providers)
+//                                Arrays.asList(
+//                                new AuthUI.IdpConfig.EmailBuilder().enableEmailLinkSignIn()
+//                                        .setActionCodeSettings(actionCodeSettings).build()))
                         .setLogo(R.drawable.ic_launcher_foreground)      // Set logo drawable
                         .setTheme(R.style.Theme_WeWell)      // Set theme
                         .setIsSmartLockEnabled(false)
                         .build(),
                 RC_SIGN_IN);
-        // [END auth_fui_create_intent]
+        System.out.println("wwwwwwwwwwwwwwwwwwwwwww");
+
     }
+    public void verifyEmail(){
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        FirebaseUser user = auth.getCurrentUser();
+        if (user.isEmailVerified()){
+            startActivity(new Intent(SignInActivity.this, MainActivity.class));
+        }else{
+            sendEmailVerification();
+        }
+    }
+
+    //Send an authentication link to the user's email address
+    public void sendEmailVerification() {
+        // [START send_email_verification]
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        FirebaseUser user = auth.getCurrentUser();
+
+        user.sendEmailVerification()
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            //Toast.makeText("email", "email sent", Toast.LENGTH_LONG);
+                            Log.d("verification", "Email sent.");
+                        }
+                    }
+                });
+        // [END send_email_verification]
+    }
+
+//    public void sendSignInLink(String email, ActionCodeSettings actionCodeSettings) {
+//        // [START auth_send_sign_in_link]
+//        FirebaseAuth auth = FirebaseAuth.getInstance();
+//        auth.sendSignInLinkToEmail(email, actionCodeSettings)
+//                .addOnCompleteListener(new OnCompleteListener<Void>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<Void> task) {
+//                        if (task.isSuccessful()) {
+//                            Log.d("signIn", "Email sent.");
+//                        }
+//                    }
+//                });
+//        // [END auth_send_sign_in_link]
+//    }
 
     // [START auth_fui_result]
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
+        System.out.println("000000000");
+        verifyEmail();
+        System.out.println("111111111");
         if (requestCode == RC_SIGN_IN) {
             IdpResponse response = IdpResponse.fromResultIntent(data);
+            System.out.println("10101010101010");
 
             if (resultCode == RESULT_OK) {
                 // Successfully signed in
@@ -87,6 +156,7 @@ public class SignInActivity extends AppCompatActivity {
                 //  But because commit() is synchronous, you should avoid calling it
                 //  from your main thread because it could pause your UI rendering.
                 editor.commit();
+
 
                 Intent intent = new Intent(SignInActivity.this, MainActivity.class);
                 startActivity(intent);
