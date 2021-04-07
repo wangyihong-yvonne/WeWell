324f54401a84c583efb9337a335ff3d9800e5b67
yu <feng.yu1@northeastern.edu>
2021-04-06 17:10:37 -0700

    add reward ui and fetching

---
+++ app/src/main/java/edu/neu/madcourse/wewell/service/RewardService.java
@@ -0,0 +1,51 @@
+package edu.neu.madcourse.wewell.service;
+
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import edu.neu.madcourse.wewell.model.Reward;
+
+public class RewardService {
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    public void getRewardsByUser(CallBack callBack, String userId) {
+        DocumentReference documentReference = db.collection("users").document(userId);
+        documentReference.get().addOnSuccessListener(documentSnapshot -> {
+            if (documentSnapshot.exists()) {
+                // todo
+                List<Map<String, Object>> rewardsFromDB = (List<Map<String, Object>>) documentSnapshot.get("rewards");
+                List<Reward> rewardList = new ArrayList<>();
+
+                for (Map<String, Object> map : rewardsFromDB) {
+                    String title = (String) map.get("title");
+                    long goal = (long) map.get("goal");
+                    long finishedDistance = (long) map.get("finishedDistance");
+                    int type = ((Number)map.get("type")).intValue();
+
+                    Reward reward = new Reward();
+                    reward.setTitle(title);
+                    reward.setGoal(goal);
+                    reward.setFinishedDistance(finishedDistance);
+                    reward.setType(type);
+
+                    rewardList.add(reward);
+                }
+
+                System.out.println(rewardList);
+                callBack.callBack(rewardList);
+            }
+        });
+    }
+
+    //use callback to retrieve data from firebase.
+    public interface CallBack {
+        void callBack(List<Reward> activityList);
+
+    }
+
+}
---
+++ app/src/main/java/edu/neu/madcourse/wewell/ui/rewards/ItemClickListener.java
@@ -0,0 +1,7 @@
+package edu.neu.madcourse.wewell.ui.rewards;
+
+public interface ItemClickListener {
+
+    void onItemClick(int position);
+
+}
